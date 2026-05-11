e7d301e207c9f7c6fc54491eb7317c87f5ad07d2
Eric  Yanchenko <ericyanchenko@Erics-MacBook-Pro-3.local>
2026-05-11 10:03:37 +0900

    Cleaned

--- sims_DCBM_K.R
+++ sims_DCBM_K.R
@@ -1,6 +1,6 @@
-## Data fission and UI for model selection on networks
-## Eric Yanchenko
-## May 1, 2025
+## Paper: Universal inference for model selection on networks
+## Author: Eric Yanchenko
+## Akita International University
 
 
 source("~/Documents/Research/network_model_selection/netcrop.R")
@@ -18,7 +18,7 @@ nreps = 100
 
 n = 1000
 alpha = 0.25
-beta.seq = seq(0, 0.8, length=21)
+beta.seq = seq(0, 0.95, length=21)
 delta=0.25
 K = 2
 
@@ -43,7 +43,7 @@ for(beta in beta.seq){
     params = list(psi=psi, B=B, C=C)
     A <- generateA("DCBM", params)
     
-    # E-value with gamma = 0.1
+    # E-value with gamma = 0.4
     # Reject if e-value is greater than 20.
     df$time[cnt] <- system.time(rej <- as.numeric(eval_mc(A, "CL", "DCBM", 2, 2, 0.4, nreps = nreps, ncores = detectCores()-1) > 20))[3]
     df$rej[cnt]  <- rej
@@ -53,7 +53,7 @@ for(beta in beta.seq){
     df$time[cnt+1] <- system.time(rej <- as.numeric(eval_mc(A, "CL", "DCBM", 2, 2, 0.5, nreps = nreps, ncores = detectCores()-1) > 20))[3]
     df$rej[cnt+1]  <- rej
     
-    # E-value with gamma = 0.9
+    # E-value with gamma = 0.6
     # Reject if e-value is greater than 20.
     df$time[cnt+2] <- system.time(rej <- as.numeric(eval_mc(A, "CL", "DCBM", 2, 2, 0.6, nreps = nreps, ncores = detectCores()-1) > 20))[3]
     df$rej[cnt+2]  <- rej
@@ -76,7 +76,7 @@ for(beta in beta.seq){
 
 load("~/Documents/Research/network_model_selection/Results/df_dcbm_K2_beta_111725.RData")
 
-df_plot <- df %>% group_by(Method, beta) %>% summarize(rej = mean(rej), time=mean(time))
+df_plot <- df %>% group_by(Method, beta) %>% summarize(rej = mean(rej, na.rm=TRUE), time=mean(time))
 
 p1 <- ggplot(df_plot, aes(x=beta, y=rej, color=Method))+
   geom_point()+
@@ -87,7 +87,6 @@ p1 <- ggplot(df_plot, aes(x=beta, y=rej, color=Method))+
 p1
 
 ggsave("~/Documents/Research/network_model_selection/Figures/dcbm_K2_beta_111725.pdf", height=4, width=6, unit="in")
-
 
 
 p2 <- ggplot(df_plot, aes(x=beta, y=time, color=Method))+
@@ -105,9 +104,8 @@ ggarrange(p1, p2, ncol=2, common.legend = TRUE, legend="bottom")
 
 n = 1000
 alpha = 0.25
-beta = 0.5
+beta = 0.7
 delta.seq = seq(0.01, 0.5, length=20)
-pp = 0.5
 K = 2
 
 B = alpha * (beta*diag(K) + (1-beta)*matrix(1, K, K))
@@ -171,7 +169,7 @@ for(delta in delta.seq){
 
 load("~/Documents/Research/network_model_selection/Results/df_dcbm_K2_delta_111725.RData")
 
-df_plot <- df %>% group_by(Method, delta) %>% summarize(rej = mean(rej), time=mean(time))
+df_plot <- df %>% group_by(Method, delta) %>% summarize(rej = mean(rej, na.rm=TRUE), time=mean(time))
 
 p1 <- ggplot(df_plot, aes(x=delta, y=rej, color=Method))+
   geom_point()+
@@ -198,10 +196,9 @@ ggarrange(p1, p2, ncol=2, common.legend = TRUE, legend="bottom")
 ## Increasing community size
 
 n = 1000
-alpha = 0.75
-beta = 0.5
+alpha = 0.90
+beta = 0.90
 delta.seq = seq(0.01, 0.20, length=20)
-pp = 0.5
 K = 5
 
 B = alpha * (beta*diag(K) + (1-beta)*matrix(1, K, K))
